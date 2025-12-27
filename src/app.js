ded55c599e4a8790e4914a70f778184a9fc375cf
Prem Prasad <premrinkinoniya@gmail.com>
2025-12-27 12:13:17 +0530

    updated backend setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const cors=require('cors');
+const {Server}=require('socket.io');
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health",(req,res)=>{
+  res.json({status:"OK"});
+})
+
+const http=require('http');
+const server=http.createServer(app);
